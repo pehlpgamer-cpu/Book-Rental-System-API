daab4194e5e54fd4cbab7d3cd03e648984b47ee6
Paulo <pehlpgamer@gmail.com>
2026-01-11 10:15:49 +0800

    bruno API test collection

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,10 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
+Route::get('/book1', function () {
+    return ['message' => 'hello'];
+});
+
+Route::controller(BookController::class)->group(function () {
+    Route::get('/books', 'index');
+});
